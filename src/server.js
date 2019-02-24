cce59e847890828e578486859bb9c34dfe0d40fa
Vinícius Filenga - VFI <vinicius.filenga@iteris.local>
2019-02-24 17:26:11 -0300

    second commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production' // Development, Production, Testing
+
+    //  Make sure to follow the exact calling order bellow
+    this.middlewares()
+    this.views()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.urlencoded({ extended: false }))
+  }
+
+  views () {
+    // __dirname is equivalent to the current file's directory
+    //  'app' and 'views' are folder to be accessed in the exact order declared here
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      watch: this.isDev,
+      express: this.express,
+      autoescape: true
+    })
+
+    this.express.set('view engine', 'njk')
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+//  exports a new instance of 'App' together with its express instance
+module.exports = new App().express
